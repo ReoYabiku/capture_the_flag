a849186c472bd5f965cd5663fa9cec361c9c8886
ReoYabiku <alcremikyu.z@gmail.com>
2023-06-23 20:54:27 +0900

    onion

--- ksnctf/onion.go
+++ ksnctf/onion.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	data, err := os.ReadFile("ksnctf/onion.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for i := 0; i < 16; i++ {
+		decoded, err := base64.StdEncoding.DecodeString(string(data))
+		if err != nil {
+			fmt.Println(err)
+		}
+		data = decoded
+	}
+
+	fmt.Println(string(data))
 }
